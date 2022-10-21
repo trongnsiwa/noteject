a70924dbf654b76923b8282ee5c8053241acc423
Si Trong <trongnsiwa79@gmail.com>
2022-10-21 16:20:53 +0700

    feat: date-fns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import {
   NbLayoutModule,
   NbButtonModule,
   NbSidebarModule,
-  NbIconModule,
 } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -17,7 +16,6 @@ import { StoreModule } from '@ngrx/store';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentsModule } from './components/components.module';
-import { CalendarComponent } from './pages/calendar/calendar.component';
 import { PageModule } from './pages/page.module';
 import { NbMenuModule } from '@nebular/theme';
 
--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { nextSunday, previousSunday } from 'date-fns';
+import {
+  format,
+  getWeekOfMonth,
+  previousMonday,
+  nextMonday,
+  getDate,
+  getDay,
+  setDate,
+} from 'date-fns';
 
 @Component({
   selector: 'app-calendar',
@@ -6,30 +16,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent implements OnInit {
-  days: any[] = [
-    {
-      day: 'Monday',
-      date: '01',
-      month: '01',
-      year: '2021',
-      works: [
-        {
-          title: 'Work 1 Work 1 Work 1 Work 1 Work 1 Work 1 Work 1 Work 1',
-          isDone: false,
-        },
-        { title: 'Work 2', isDone: false },
-        { title: 'Work 3', isDone: true },
-      ],
-    },
-    { day: 'Tuesday', date: '02', month: '01', year: '2021', works: [] },
-    { day: 'Wednesday', date: '03', month: '01', year: '2021', works: [] },
-    { day: 'Thursday', date: '04', month: '01', year: '2021', works: [] },
-    { day: 'Friday', date: '05', month: '01', year: '2021', works: [] },
-    { day: 'Saturday', date: '06', month: '01', year: '2021', works: [] },
-    { day: 'Sunday', date: '07', month: '01', year: '2021', works: [] },
-  ];
+  options: {
+    weekStartsOn?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | undefined;
+  } = {
+    weekStartsOn: 1,
+  };
+  currentDate: Date = new Date();
+  current = {
+    week: getWeekOfMonth(new Date(), this.options),
+    monthAndYear: format(new Date(), 'MMMM yyyy', this.options),
+    dayOfWeek: format(new Date(), 'EEEE, dd/MM/yyyy', this.options),
+  };
+  week: any[] = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.generateWeek();
+  }
+
+  generateWeek() {
+    for (let i = 1; i <= 7; i++) {
+      let curr = new Date(this.currentDate);
+      let first = getDate(curr) - getDay(curr) + i;
+      let day = setDate(curr, first);
+      let date = new Date(day);
+
+      this.week.push({
+        dayOfWeek: format(date, 'EEEE', this.options),
+        date: format(date, 'dd/MM/yyyy', this.options),
+        works: [],
+        isCurrentDate:
+          format(date, 'dd/MM/yyyy', this.options) ===
+          format(new Date(), 'dd/MM/yyyy', this.options),
+      });
+    }
+  }
+
+  nextWeek() {
+    const date = nextSunday(this.currentDate);
+
+    this.currentDate = date;
+    this.current = {
+      ...this.current,
+      week: getWeekOfMonth(date, this.options),
+      monthAndYear: format(date, 'MMMM yyyy', this.options),
+    };
+    this.week = [];
+    this.generateWeek();
+  }
+
+  prevWeek() {
+    const date = previousSunday(this.currentDate);
+
+    this.currentDate = date;
+    this.current = {
+      ...this.current,
+      week: getWeekOfMonth(date, this.options),
+      monthAndYear: format(date, 'MMMM yyyy', this.options),
+    };
+    this.week = [];
+    this.generateWeek();
+  }
 }
