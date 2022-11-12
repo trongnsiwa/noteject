c65861e88ce472a07ebafbe5f23474c9e2a7cdfa
Si Trong <trongnsiwa79@gmail.com>
2022-11-12 22:12:54 +0700

    feat: validation form

--- src/app/auth/pages/signup/signup.component.ts
+++ src/app/auth/pages/signup/signup.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { matchValidator } from '../../../core/utils/form-validators';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        Validators.maxLength(50),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(40),
+        matchValidator('confirmPassword', true),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        matchValidator('password'),
+      ]),
+    });
   }
 
+  get username() {
+    return this.signupForm.get('username');
+  }
+
+  get password() {
+    return this.signupForm.get('password');
+  }
+
+  get email() {
+    return this.signupForm.get('email');
+  }
+
+  get confirmPassword() {
+    return this.signupForm.get('confirmPassword');
+  }
+
+  signup() {}
 }
